e2004c35e4a587fe3fc8a778e7652e0557297738
Andrew Bruce <andrew@camelpunch.com>
2013-05-24 00:49:21 -0700

    project.clj whitespace

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject compojureref "0.1.0-SNAPSHOT"
+(defproject
+  compojureref "0.1.0-SNAPSHOT"
   :description "Just a bunch of Compojure tricks"
   :url "https://github.com/camelpunch/compojureref"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.3"]]
+                 [hiccup "1.0.3"]
+                 ]
   :plugins [
             [lein-ring "0.8.3"]
             [lein-expectations "0.0.7"]
